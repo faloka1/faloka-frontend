852aef84eb5fa5f1a8fce6d10ed0cf7eb4f62b9a
dryluigi <marioprasetyamulya85@gmail.com>
2021-09-28 16:10:55 +0700

    feat: call login API
    also implement all login features. this is including:
    - logout
    - is logged in check
    - error in response
    Fixes #18

--- src/pages/Auth/Login/Login.js
+++ src/pages/Auth/Login/Login.js
@@ -1,32 +1,85 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { Form, Button, Container} from 'react-bootstrap';
-import { useFormik} from 'formik';
-import * as Yup from 'yup';
 
-import { ReactComponent as EmailIcon } from '../../../components/SVG/email.svg';
-import { ReactComponent as PasswordIcon } from '../../../components/SVG/key.svg';
+import React, { useState, useEffect } from 'react';
+import { Link, useHistory, Prompt } from 'react-router-dom';
+import { Form, Button, Container, Spinner } from 'react-bootstrap';
+import { useDispatch } from 'react-redux';
+import { useFormik } from 'formik';
+import { useMutation } from 'react-query';
+import * as Yup from 'yup';
 
 import './Login.scss';
 
+import { ReactComponent as EmailIcon } from '../../../components/SVG/email.svg';
+import { ReactComponent as PasswordIcon } from '../../../components/SVG/key.svg';
+import { login } from '../../../stores/auth/auth-actions';
+import axios from 'axios';
+
+const loginURL = "http://192.168.100.7:8000/api/auth/login";
+
 const Login = () => {
+  const [formIsFilled, setFormIsFilled] = useState(false);
+  const [isInitial, setIsInitial] = useState(true);
+  const [errorMessage, setErrorMessage] = useState('');
+  const dispatch = useDispatch();
+  const history = useHistory();
+
+  const loginMutation = useMutation(async loginData => {
+    try {
+      const response = await axios.post(loginURL, loginData);
+
+      return response.data;
+    } catch (error) {
+      if (error.response) {
+        setErrorMessage('Login gagal. Pastikan email dan password benar!');
+        throw new Error(JSON.stringify(error.response.data));
+      } else {
+        setErrorMessage('Terjadi kesalahan! Silakan coba sesaat lagi.');
+        throw new Error();
+      }
+    }
+  }, {
+    onSuccess: (result, variables, context) => {
+      dispatch(login(result.access_token, result.expires_in));
+
+      history.replace('/');
+    },
+    onError: (error, variables, context) => {
+      console.log(error);
+    },
+  });
+
   const formValidation = useFormik({
     initialValues: {
       email: '',
       password: ''
     },
-
     validationSchema: Yup.object({
       email: Yup.string().email('Invalid email address.').required('Email required.'),
       password: Yup.string().required('Password required.')
     }),
-
     onSubmit: values => {
-      alert(JSON.stringify(values, null, 2));
+      const loginData = values;
+      loginMutation.mutate(loginData);
     },
   });
+
+  useEffect(() => {
+    if (!isInitial && !formIsFilled) {
+      setFormIsFilled(true);
+    }
+  }, [formValidation.values.email, formValidation.values.password]);
+
+  useEffect(() => {
+    setIsInitial(false);
+  }, []);
+
+
   return (
     <>
+      <Prompt
+        when={formIsFilled}
+        message="You have unsaved changes, are you sure you want to leave?"
+      />
       <Container className="d-flex align-items-center justify-content-center">
         <div className="login-form">
           <Form onSubmit={formValidation.handleSubmit}>
@@ -34,13 +87,13 @@ const Login = () => {
             <Form.Group className="mb-3">
               <div className="inner-addon left-addon">
                 <div className="icon-placeholder">
-                  <EmailIcon className="icon"/>
+                  <EmailIcon className="icon" />
                 </div>
-                <Form.Control id="email" name="email" type="email" placeholder="Masukan Email" 
-                  onChange={formValidation.handleChange} 
-                  onBlur={formValidation.handleBlur} 
+                <Form.Control id="email" name="email" type="email" placeholder="Masukan Email"
+                  onChange={formValidation.handleChange}
+                  onBlur={formValidation.handleBlur}
                   value={formValidation.values.email}
-                  className={`${formValidation.touched.email && 
+                  className={`${formValidation.touched.email &&
                     formValidation.errors.email ? ("login-error") : ``}`}
                 />
               </div>
@@ -51,13 +104,13 @@ const Login = () => {
             <Form.Group className="mb-2">
               <div className="inner-addon left-addon">
                 <div className="icon-placeholder">
-                  <PasswordIcon className="icon"/>
+                  <PasswordIcon className="icon" />
                 </div>
-                <Form.Control id="password" name="password" type="password" placeholder="Masukan Password" 
-                  onChange={formValidation.handleChange} 
-                  onBlur={formValidation.handleBlur} 
+                <Form.Control id="password" name="password" type="password" placeholder="Masukan Password"
+                  onChange={formValidation.handleChange}
+                  onBlur={formValidation.handleBlur}
                   value={formValidation.values.password}
-                  className={`${formValidation.touched.password && 
+                  className={`${formValidation.touched.password &&
                     formValidation.errors.password ? ("login-error") : ``}`}
                 />
               </div>
@@ -69,7 +122,18 @@ const Login = () => {
               <Link to="/forgot">Lupa Password?</Link>
             </Form.Text>
             <div className="d-grid mt-5">
-              <Button variant={'primary'} type={'submit'} className={'btn-flat'}>Masuk</Button>
+              <Button
+                variant={'primary'}
+                type={'submit'}
+                className={'btn-flat'}
+                disabled={loginMutation.isLoading}>
+                {loginMutation.isLoading
+                  ? (<Spinner animation="border" role="status" className="login-loading">
+                    <span className="visually-hidden">Loading...</span>
+                  </Spinner>)
+                  : 'Masuk'}
+              </Button>
+              {loginMutation.isError && <small className="login-error text-center">{errorMessage}</small>}
             </div>
           </Form>
           <div className="form-footer">
