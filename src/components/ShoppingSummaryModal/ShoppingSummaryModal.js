a296be6545dbc104948c382f6f501fb38a26b91e
dryluigi <marioprasetyamulya85@gmail.com>
2021-10-17 22:00:10 +0700

    feat: display correct info in shopping summary Fixes #74

--- src/components/ShoppingSummaryModal/ShoppingSummaryModal.js
+++ src/components/ShoppingSummaryModal/ShoppingSummaryModal.js
@@ -1,17 +1,31 @@
 import React, { useContext } from 'react'
 import { Modal } from 'react-bootstrap';
+import CurrencyFormat from 'react-currency-format';
 import { Link } from 'react-router-dom';
 import { CheckoutContext } from '../../context/CheckoutContext/CheckoutContext';
+import concatAddress from '../../helpers/concat-address';
 import ProductsSummary from '../ProductsSummary/ProductsSummary';
 
 import './ShoppingSummaryModal.scss';
 
 const ShoppingSummaryModal = ({ closeFunc, ...props }) => {
-  const { isConfirmed, setIsConfirmed, product, paymentMethod } = useContext(CheckoutContext);
+  const {
+    isConfirmed,
+    setIsConfirmed,
+    product,
+    paymentMethod,
+    expedition,
+    shipmentAddress
+  } = useContext(CheckoutContext);
 
-  if (!!!paymentMethod) {
+  if (!!!paymentMethod || !!!product.product || !!!expedition.cost) {
     return null;
   }
+
+  const itemPrice = product.product.price;
+  const quantity = product.quantity;
+  const itemsPrice = itemPrice * quantity;
+  const shipmentCost = expedition.cost.cost[0].value;
 
   const closeModal = () => {
     if (closeFunc) {
@@ -42,46 +56,74 @@ const ShoppingSummaryModal = ({ closeFunc, ...props }) => {
         </section>
         <section className="summary-section">
           <div className="summary-section__container">
-            <p className="h5 mb-3 summary-section__title">Pengiriman</p>
-            <div className="d-flex justify-content-between">
-              <p className="text-gray">Alamat</p>
-              <p className="fw-bold">Rungkut Asri Utara 15 No. 19</p>
+            <p className="h5 mb-2 summary-section__title border-bottom pb-2">Pengiriman</p>
+            <div className="d-flex justify-content-between mb-1">
+              <p className="text-gray mb-0">Metode</p>
+              <p className="fw-bold mb-0">{expedition.name}</p>
             </div>
-            <div className="d-flex justify-content-between">
-              <p className="text-gray">Metode</p>
-              <p className="fw-bold">JNE</p>
+            <div className="d-flex justify-content-between border-bottom pb-2">
+              <p className="text-gray mb-0">Type</p>
+              <p className="fw-bold mb-0">{expedition.cost.service} ({expedition.cost.description})</p>
             </div>
-            <div className="d-flex justify-content-between">
-              <p className="text-gray">Tipe</p>
-              <p className="fw-bold">Express</p>
-            </div>
-            <div className="d-flex justify-content-between">
-              <p className="text-gray">Ongkos Kirim</p>
-              <p className="fw-bold text-accent">Rp13.000,-</p>
+            <p className="text-gray my-2">Alamat</p>
+            <p className="fw-bold">{shipmentAddress.name}</p>
+            <p>{concatAddress(shipmentAddress.address)}</p>
+            <p>{shipmentAddress.phoneNumber}</p>
+            <div className="d-flex justify-content-between border-top pt-2">
+              <p className="text-uppercase mb-0">Ongkos Pengiriman</p>
+              <CurrencyFormat
+                value={shipmentCost}
+                displayType={'text'}
+                prefix={'Rp'}
+                thousandSeparator="."
+                decimalSeparator=","
+                renderText={value => <p className="fw-bold text-accent mb-0">{value}</p>}
+              />
             </div>
           </div>
         </section>
         <section className="summary-section">
           <div className="summary-section__container">
-            <p className="h5 mb-3 summary-section__title">Pembayaran</p>
+            <p className="h5 mb-2 summary-section__title border-bottom pb-2">Pembayaran</p>
             <div className="d-flex justify-content-between">
               <p className="text-gray">Metode Pembayaran</p>
               <div>
                 <p className="fw-bold text-end mb-0">Transfer {paymentMethod.payment_name}</p>
-                <p>{paymentMethod.no_rekening} ({paymentMethod.payment_name})</p>
+                <p>{paymentMethod.no_rekening} ({paymentMethod.account_name})</p>
               </div>
             </div>
             <div className="d-flex justify-content-between">
-              <p className="text-gray">Subtotal ( 1 item )</p>
-              <p className="fw-bold">JNE</p>
+              <p className="text-gray">Subtotal ( {quantity} item )</p>
+              <CurrencyFormat
+                value={itemsPrice}
+                displayType={'text'}
+                prefix={'Rp'}
+                thousandSeparator="."
+                decimalSeparator=","
+                renderText={value => <p className="fw-bold">{value}</p>}
+              />
             </div>
             <div className="d-flex justify-content-between">
-              <p className="text-gray">Ongkos Kirim</p>
-              <p className="fw-bold">Rp13.000,-</p>
+              <p className="text-gray">Ongkos Pengiriman</p>
+              <CurrencyFormat
+                value={shipmentCost}
+                displayType={'text'}
+                prefix={'Rp'}
+                thousandSeparator="."
+                decimalSeparator=","
+                renderText={value => <p className="fw-bold">{value}</p>}
+              />
             </div>
-            <div className="d-flex justify-content-between">
-              <p className="text-gray">Total Pembayaran</p>
-              <p className="fw-bold text-accent">Rp100.000,-</p>
+            <div className="d-flex justify-content-between border-top pt-2">
+              <p className="text-uppercase mb-0">Total Pembayaran</p>
+              <CurrencyFormat
+                value={(shipmentCost + itemsPrice)}
+                displayType={'text'}
+                prefix={'Rp'}
+                thousandSeparator="."
+                decimalSeparator=","
+                renderText={value => <p className="fw-bold text-accent mb-0">{value}</p>}
+              />
             </div>
           </div>
         </section>
