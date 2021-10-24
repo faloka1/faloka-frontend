f7ff944231eabdf2de456625996897803e2fdcbb
dryluigi <marioprasetyamulya85@gmail.com>
2021-10-24 22:23:05 +0700

    slice mix and match dashboard Fixes #82

--- src/components/MixAndMatchCanvas/MixAndMatchCanvas.js
+++ src/components/MixAndMatchCanvas/MixAndMatchCanvas.js
@@ -3,22 +3,27 @@ import Placeholder from './Placeholder/Placeholder';
 
 import './MixAndMatchCanvas.scss';
 
-const MixAndMatchCanvas = ({ className, ...props }) => {
+const MixAndMatchCanvas = ({ className, top, bottom, ...props }) => {
   let classes = 'mix-and-match-canvas border h-100 p-5 d-flex flex-row flex-lg-column justify-content-center align-items-center';
 
   if (className) {
     classes += ' ' + className;
   }
 
+  const topContent = top
+    ? <img className="w-100 h-100" src={top.image} alt="mix_and_match_outfit" />
+    : <Placeholder type="top" />
+  const bottomContent = bottom
+    ? <img className="w-100 h-100" src={bottom.image} alt="mix_and_match_outfit" />
+    : <Placeholder type="bottom" />
+
   return (
     <div className={classes}>
-      {/* <div className="mb-2" style={{ backgroundColor: 'red', width: '120px', height: '120px' }}></div> */}
-      {/* <div className="" style={{ backgroundColor: 'blue', width: '120px', height: '120px' }}></div> */}
       <div className="item mb-0 mb-lg-3 me-3 me-lg-0">
-        <Placeholder type="top" />
+        {topContent}
       </div>
       <div className="item">
-        <Placeholder type="bottom" />
+        {bottomContent}
       </div>
     </div>
   );
--- src/components/MixAndMatchMenu/MixAndMatchMenu.js
+++ src/components/MixAndMatchMenu/MixAndMatchMenu.js
@@ -5,7 +5,17 @@ import './MixAndMatchMenu.scss';
 
 import Item from './Item/Item';
 
-const MixAndMatchMenu = () => {
+const DUMMY_TOP = {
+  image: "./assets/images/products/product_3.png",
+  slug: "toppp"
+};
+
+const DUMMY_BOTTOM = {
+  image: "./assets/images/products/product_4.png",
+  slug: "bottommmmm"
+};
+
+const MixAndMatchMenu = ({ onSetTop, onSetBottom }) => {
   return (
     <div className="mix-and-match-menu border custom-tab">
       <Tabs defaultActiveKey="atasan">
@@ -13,8 +23,11 @@ const MixAndMatchMenu = () => {
           <Row className="g-4">
             {[...Array(7)].map((x, i) =>
               <>
-                <Col xs={4} key={x}>
+                <Col xs={4} key={x} onClick={() => onSetTop(DUMMY_TOP)}>
                   <Item image="./assets/images/products/product_3.png" path="#" />
+                </Col>
+                <Col xs={4} key={x} onClick={() => onSetTop({ image: "./assets/images/products/product_5.png", slug: "testtt" })}>
+                  <Item image="./assets/images/products/product_5.png" path="#" />
                 </Col>
               </>
             )}
@@ -23,9 +36,14 @@ const MixAndMatchMenu = () => {
         <Tab eventKey="bawahan" title="Bawahan">
           <Row className="g-4">
             {[...Array(14)].map((x, i) =>
-              <Col xs={4} key={x}>
-                <Item image="./assets/images/products/product_4.png" path="#" />
-              </Col>
+              <>
+                <Col xs={4} key={x} onClick={() => onSetBottom(DUMMY_BOTTOM)}>
+                  <Item image="./assets/images/products/product_4.png" path="#" />
+                </Col>
+                <Col xs={4} key={x} onClick={() => onSetBottom({ image: "./assets/images/products/product_6.png", slug: "tesdwwdd" })}>
+                  <Item image="./assets/images/products/product_6.png" path="#" />
+                </Col>
+              </>
             )}
           </Row>
         </Tab>
