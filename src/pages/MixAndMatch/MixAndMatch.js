4da0d5299fc61e76d3afe8938bea1cc033f7b17b
Ilham Santosa <ilham.boys20@gmail.com>
2021-12-03 00:37:34 +0700

    feat: Add mannequin model Fixes #83

--- src/pages/MixAndMatch/MixAndMatch.js
+++ src/pages/MixAndMatch/MixAndMatch.js
@@ -13,7 +13,7 @@ const MixAndMatch = () => {
       <h2 className="text-center h4 my-4">Buat Mix and Matchmu</h2>
       <Row className="g-4">
         <Col xs={12} lg={7}>
-          <MixAndMatchCanvas top={top} bottom={bottom} />
+          <MixAndMatchCanvas top={top} bottom={bottom} setTop={setTop} setBottom={setBottom}/>
         </Col>
         <Col xs={12} lg={5}>
           <MixAndMatchMenu onSetTop={setTop} onSetBottom={setBottom} />
