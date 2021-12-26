6396c5a9f7053f662119981d82399a597642085c
Mario Prasetya Mulya <marioprasetyamulya85@gmail.com>
2021-12-26 14:13:30 +0700

    feat: make item on user dropdown left aligned Fixes #152

--- src/components/Navbar/NavDropdown/NavDropdown.js
+++ src/components/Navbar/NavDropdown/NavDropdown.js
@@ -3,11 +3,13 @@ import { Container } from 'react-bootstrap';
 
 import './NavDropdown.scss';
 
-const NavDropdown = ({ children, centered, className }) => {
+const NavDropdown = ({ children, centered, className, align }) => {
   let classes = 'nav-dropdown top-100';
 
-  if (centered) {
+  if (align === 'center') {
     classes += ' start-50 translate-middle-x';
+  } else if (align === 'right') {
+    classes += ' end-0';
   } else {
     classes += ' start-0';
   }
